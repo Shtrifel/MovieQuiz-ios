17be03988f6c304540e6dfe5056b38c5b180ae03
Shtrifel <119096060+Shtrifel@users.noreply.github.com>
2023-01-17 16:36:48 +0300

    Revert "Sprint 05"

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,10 +1,4 @@
+import Foundation
 import UIKit
 
-extension UIColor {
-           static var ypGreen: UIColor { UIColor(named: "YP Green (iOS)")!}
-           static var ypRed: UIColor { UIColor(named: "YP Red (iOS)")!}
-           static var ypBlack: UIColor { UIColor(named: "YP Black (iOS)")!}
-           static var ypWhite: UIColor { UIColor(named: "YP White (iOS)")!}
-           static var ypGray: UIColor { UIColor(named: "YP Gray (iOS)")!}
-           static var ypBackground: UIColor { UIColor(named: "YP Background (iOS)")!}
-}
+extension UIColor { }
